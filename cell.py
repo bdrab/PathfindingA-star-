efd3489b4d1ea401d042ffe9c0b2b48ddf32b7bd
bdrab <bartekdrab4@wp.pl>
2022-06-16 20:42:20 +0200

    update

--- cell.py
+++ cell.py
@@ -1,9 +1,13 @@
 import pygame
 
-WHITE = (255, 255, 255)
+
 BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165, 0)
+WHITE = (255, 255, 255)
 
 
 class Cell:
@@ -24,7 +28,7 @@ class Cell:
         self.color = BLUE
 
     def make_cell_end(self):
-        self.color = RED
+        self.color = ORANGE
 
     def make_empty(self):
         self.color = WHITE
@@ -38,7 +42,21 @@ class Cell:
         else:
             return False
 
+    def get_pos(self):
+        return self.row, self.column
+
+    def make_open(self):
+        self.color = GREEN
+
+    def make_closed(self):
+        self.color = RED
+
+    def make_path(self):
+        self.color = PURPLE
+
     def update_neighbours(self, grid):
+        self.neighbours.clear()
+
         if self.row > 0 and not grid[self.row-1][self.column].is_barrier():
             self.neighbours.append(grid[self.row-1][self.column])
 
--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from cell import Cell
 import pygame
+from queue import PriorityQueue
 
 WIN_SIZE = 800
-CELL_SIZE = 80
+CELL_SIZE = 40
 rows = WIN_SIZE // CELL_SIZE
 grid = []
 
 pygame.init()
 screen = pygame.display.set_mode((WIN_SIZE, WIN_SIZE))
 pygame.display.set_caption("A* Algorithm")
+
+
+def make_cell_grid():
+    for row_grid in range(rows):
+        grid.append([])
+        for column_grid in range(rows):
+            new_cell = Cell(row_grid, column_grid, CELL_SIZE, rows)
+            grid[row_grid].append(new_cell)
 
 
 def draw_lines():
@@ -24,30 +33,74 @@ def draw_cells():
             cell.draw(screen)
 
 
-def make_cell_grid():
-    for row_grid in range(rows):
-        grid.append([])
-        for column_grid in range(rows):
-            new_cell = Cell(row_grid, column_grid, CELL_SIZE, rows)
-            grid[row_grid].append(new_cell)
-
-
-make_cell_grid()
-end_program = False
-start, end = None, None
-
-
 def calculate_distance(cell1, cell2):
     x1, y1 = cell1
     x2, y2 = cell2
     return abs(x1 - x2) + abs(y1 - y2)
 
 
+def reconstruct_path(came_from, current):
+    while current in came_from:
+        current = came_from[current]
+        current.make_path()
+
+
+def start_algorithm(grid_cell, start_cell, end_cell):
+    count_value = 0
+    open_set = PriorityQueue()
+    open_set.put((0, count_value, start_cell))
+    came_from = {}
+    g_score = {spot: float("inf") for row_in_grid in grid_cell for spot in row_in_grid}
+    g_score[start_cell] = 0
+    f_score = {spot: float("inf") for row_in_grid in grid_cell for spot in row_in_grid}
+    f_score[start_cell] = calculate_distance(start_cell.get_pos(), end_cell.get_pos())
+
+    open_set_copy = {start_cell}
+
+    while not open_set.empty():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        current_cell = open_set.get()[2]
+        open_set_copy.remove(current_cell)
+
+        if current_cell == end_cell:
+            reconstruct_path(came_from, end_cell)
+            start_cell.make_cell_start()
+            end_cell.make_cell_end()
+            return True
+
+        for neighbour in current_cell.neighbours:
+            temp_g_score = g_score[current_cell] + 1
+
+            if temp_g_score < g_score[neighbour]:
+                came_from[neighbour] = current_cell
+                g_score[neighbour] = temp_g_score
+                f_score[neighbour] = temp_g_score + calculate_distance(neighbour.get_pos(), end_cell.get_pos())
+                if neighbour not in open_set_copy:
+                    count_value += 1
+                    open_set.put((f_score[neighbour], count_value, neighbour))
+                    open_set_copy.add(neighbour)
+                    neighbour.make_open()
+
+        draw_cells()
+        draw_lines()
+        pygame.display.flip()
+
+        if current_cell != start_cell:
+            current_cell.make_closed()
+    return False
+
+
+make_cell_grid()
+end_program = False
+start, end = None, None
+
 while not end_program:
     mouse_moved = False
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
             end_program = True
 
         elif pygame.mouse.get_pressed()[0]:
@@ -73,8 +126,22 @@ while not end_program:
 
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE and start and end:
-                print("Algorithm started")
+                for row in grid:
+                    for cell in row:
+                        cell.update_neighbours(grid)
+
+                if start_algorithm(grid, start, end):
+                    print("Path has been found.")
+                else:
+                    print("There is no path to destination point.")
+
+            elif event.key == pygame.K_r:
+                for row in grid:
+                    for cell in row:
+                        cell.make_empty()
+                start, end = None, None
 
     draw_cells()
     draw_lines()
     pygame.display.flip()
+pygame.quit()
